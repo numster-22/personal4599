029023461bd9a2470214e7248fd16f6f6e1e6a12
numster22 <arizmendi5022@gmail.com>
2024-07-10 13:58:17 -0400

    Add starter files for 00-intro

---
+++ 00-intro/intro.js
@@ -0,0 +1,5 @@
+// This is the JavaScript code
+// GUIDELINES:
+//  1. Give every variable/const smallest scope
+//  2. Grab HTML elements by tag and store in const
+//  3. Put all other code inside functions
