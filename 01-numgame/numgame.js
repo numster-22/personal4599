483c93fb92e6435e59c1f2dc9ef4f07c42d32e03
numster22 <arizmendi5022@gmail.com>
2024-07-11 10:11:54 -0400

    add guessing button

---
+++ 01-numgame/numgame.js
@@ -0,0 +1,40 @@
+// GUIDELINES:
+//  1. Give every variable/const smallest scope
+//  2. Grab HTML elements by tag and store in const
+//  3. Put all other code inside functions
+
+const numField = document.getElementById('num-field');
+const messageText = document.getElementById('message-text');
+
+
+let secret;
+let min=1;
+let max=100;
+
+var myConfetti = confetti.create(null, {
+    resize: true,
+    useWorker: true
+});
+
+function loadGame(){
+    secret = ; //TODO: make the number random
+}
+
+function makeGuess() {
+    let guess = parseInt(numField.value);
+    console.log(`Guess: ${guess}`);
+    if (guess < secret){
+        messageText.innerHTML = `${guess} is too low`;
+    } else if(guess > secret){
+        messageText.innerHTML = `${guess} is too high`;
+    } else if(guess == secret) {
+        messageText.innerHTML = `${guess} is Correct!, You Won!`;
+        myConfetti({
+            particleCount:300,
+            spread: 360
+        });
+    } else {
+        messageText.innerHTML = `No guess`;
+    }
+    
+}
